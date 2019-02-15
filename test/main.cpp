4e87227c21e41f8486b6815003145242d3c2a6bb
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-15 16:49:17 +0100

    Add: Some more tests for DistanceOnCircle

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,4 @@
+// Copyright 2019 Erik Teichmann <kontakt.teichmann@gmail.com>
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
--- test/test_math.cpp
+++ test/test_math.cpp
@@ -1,17 +1,24 @@
-#include "catch.hpp"
+// Copyright 2019 Erik Teichmann <kontakt.teichmann@gmail.com>
 
 #include <cmath>
-
+#include "catch.hpp"
 #include <toolbox/math.hpp>
 
 
 namespace tb = toolbox;
 
 TEST_CASE("distance on the circle") {
+  // test invariance of input ordering
   REQUIRE(tb::DistanceOnCircle(0., M_PI/2.) == Approx(M_PI/2.));
   REQUIRE(tb::DistanceOnCircle(M_PI/2., 0.) == Approx(M_PI/2.));
 
+  // test invariance to location
+  REQUIRE(tb::DistanceOnCircle(M_PI, M_PI/2.) == Approx(M_PI/2.));
+  REQUIRE(tb::DistanceOnCircle(M_PI/2., M_PI) == Approx(M_PI/2.));
+
+  // test edgecase
   REQUIRE(tb::DistanceOnCircle(0., M_PI) == Approx(M_PI));
 
+  // test short distance
   REQUIRE(tb::DistanceOnCircle(3.*M_PI/2., 0.) == Approx(M_PI/2.));
 }
