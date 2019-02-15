78a2dc252b9f74892fed1eb5bda2450ac200b258
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-15 16:05:26 +0100

    Update: Restructure src and optimize tests

--- src/math.cpp
+++ src/math.cpp
@@ -2,6 +2,8 @@
 
 namespace tb = toolbox;
 
+// Calculate the distance first counterclockwise, then the complement
+// and choose the smalles one.
 double tb::DistanceOnCircle(double phase_1, double phase_2) {
   auto distance_1 = std::max(phase_1, phase_2) - std::min(phase_1, phase_2);
   auto distance_2 = 2*M_PI - distance_1;
